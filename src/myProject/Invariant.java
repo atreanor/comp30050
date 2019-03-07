8f37a72a44c4bacb7fa7372c3a886c451ad86a16
Alan Treanor <alan.treanor@ucdconnect.ie>
2019-03-07 16:45:57 +0000

    Invariant Class added

---
+++ src/myProject/Invariant.java
@@ -0,0 +1,10 @@
+package myProject;
+
+import java.lang.annotation.RetentionPolicy;
+
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Invariant {
+
+}
